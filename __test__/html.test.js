3659ddb090e38a618270f3db55eaf89b2f03f74f
Ivan Lisin <77109733+ivanlisin@users.noreply.github.com>
2021-01-25 15:28:14 +0700

    update html module

--- __test__/html.test.js
+++ __test__/html.test.js
@@ -1,16 +1,37 @@
 import path from 'path';
 import { promises as fs } from 'fs';
-import { extractLinks } from '../src/html';
+import cheerio from 'cheerio';
+import { makeDirNameByUrl, makeFileNameByUrl } from '../src/name';
+import { getSourceLinks, replaceSrcLinksOnFilePaths } from '../src/html';
 
 const readFixtureFile = async (filepath) => {
   const fixtureFilePath = path.join('__fixtures__', filepath);
   return fs.readFile(fixtureFilePath, 'utf-8');
 };
 
-test('get links from html', async () => {
+test('get source links from html', async () => {
   const data = await readFixtureFile('index.html');
   const { url, srcLinks: expected } = JSON.parse(await readFixtureFile('page-loader-config.json'));
   const { origin } = new URL(url);
-  const actual = extractLinks(data, origin);
+  const actual = getSourceLinks(data, origin);
   expect(actual.sort()).toEqual(expected.sort());
+});
+
+const getHtml = async (...pathSegments) => {
+  const filepath = path.join('__fixtures__', ...pathSegments);
+  const data = await fs.readFile(filepath, 'utf-8');
+  return cheerio.load(data).html();
+};
+
+test('replace src links on filepaths', async () => {
+  const html = await getHtml('index.html');
+
+  const { url } = JSON.parse(await readFixtureFile('page-loader-config.json'));
+  const { origin } = new URL(url);
+  const expected = await getHtml('expected-dir', 'ru-hexlet-io-courses.html');
+
+  const srcDir = makeDirNameByUrl(url, origin);
+  const makeFilePathByUrl = (innerUrl) => path.join(srcDir, makeFileNameByUrl(innerUrl, origin));
+
+  expect(replaceSrcLinksOnFilePaths(html, origin, makeFilePathByUrl)).toEqual(expected);
 });
--- src/html.js
+++ src/html.js
@@ -2,9 +2,8 @@ import cheerio from 'cheerio';
 
 const tagAndResourceAttributeMapping = { img: 'src', link: 'href', script: 'src' };
 
-// eslint-disable-next-line import/prefer-default-export
-export const extractLinks = (data, origin) => {
-  const $ = cheerio.load(data);
+export const getSourceLinks = (html, origin) => {
+  const $ = cheerio.load(html);
   return Object.entries(tagAndResourceAttributeMapping)
     .map(([tag, resAttr]) => $(tag).map((i, el) => $(el).attr(resAttr)).toArray())
     .flat(1)
@@ -12,4 +11,20 @@ export const extractLinks = (data, origin) => {
       const { href } = new URL(url, origin);
       return href.match(new RegExp(origin));
     });
+};
+
+export const replaceSrcLinksOnFilePaths = (html, origin, makeFilePathByUrl) => {
+  const $ = cheerio.load(html);
+  Object.entries(tagAndResourceAttributeMapping)
+    .forEach(([tag, resAttr]) => {
+      $(tag).each((i, el) => {
+        const link = $(el).attr(resAttr);
+        const { href } = new URL(link, origin);
+        if (href.match(new RegExp(origin))) {
+          const filepath = makeFilePathByUrl(link, origin);
+          $(el).attr(resAttr, filepath);
+        }
+      });
+    });
+  return $.html();
 };
