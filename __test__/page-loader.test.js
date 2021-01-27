2a39ce9df2453fd7df876e3004705ea16c0197a1
Ivan Lisin <77109733+ivanlisin@users.noreply.github.com>
2021-01-27 16:57:45 +0700

    update page loader test

--- __test__/page-loader.test.js
+++ __test__/page-loader.test.js
@@ -6,23 +6,17 @@ import { promises as fs } from 'fs';
 import nock from 'nock';
 import loadPage from '../src/page-loader';
 
+const url = 'https://ru.hexlet.io/courses';
 let tmpdir;
-let url;
-let filepaths;
 
 const defineTmpDir = async () => {
   const dirpath = path.join(os.tmpdir(), 'page-loader-');
   tmpdir = await fs.mkdtemp(dirpath);
 };
 
-const getConfig = async (configName) => {
-  const fixtureFilePath = path.join('__fixtures__', configName);
-  const data = await fs.readFile(fixtureFilePath, 'utf-8');
-  return JSON.parse(data);
-};
-
 const turnOnNock = async () => {
-  const nockConfig = await getConfig('nock-config.json');
+  const configPath = path.join('__fixtures__', 'nock-config.json');
+  const nockConfig = JSON.parse(await fs.readFile(configPath, 'utf-8'));
   const promises = nockConfig.map(async ({ urlbase, urlpath, filepath }) => {
     const data = await fs.readFile(filepath, 'utf-8');
     nock(urlbase).get(urlpath).reply(200, data);
@@ -30,37 +24,37 @@ const turnOnNock = async () => {
   await Promise.all(promises);
 };
 
-const definePageLoaderTestData = async () => {
-  const pageLoaderConfig = await getConfig('page-loader-config.json');
-  url = pageLoaderConfig.url;
-  filepaths = pageLoaderConfig.filepaths;
-};
-
 beforeAll(async () => {
   await defineTmpDir();
   await turnOnNock();
-  await definePageLoaderTestData();
   await loadPage(url, tmpdir);
 });
 
-test('match actual and expected direcory size', async () => {
+test('match actual and expected directory size', async () => {
   const actualStat = await fs.stat(tmpdir);
   const expectedDir = path.join('__fixtures__', 'expected-dir');
   const expectedStat = await fs.stat(expectedDir);
   expect(actualStat.size).toBe(expectedStat.size);
 });
 
-const readExpectedFile = async (filepath) => {
+const readActualFile = (filepath) => {
+  const actualFile = path.join(tmpdir, filepath);
+  return fs.readFile(actualFile, 'utf-8');
+};
+
+const readExpectedFile = (filepath) => {
   const fixtureFilePath = path.join('__fixtures__', 'expected-dir', filepath);
   return fs.readFile(fixtureFilePath, 'utf-8');
 };
 
-test('match actual and expected files', async () => {
-  const promises = filepaths.map(async (filepath) => {
-    const actualFile = path.join(tmpdir, filepath);
-    const actual = await fs.readFile(actualFile, 'utf-8');
-    const expected = await readExpectedFile(filepath);
-    expect(actual).toBe(expected);
-  });
-  await Promise.all(promises);
+test.each([
+  ['ru-hexlet-io-courses.html'],
+  ['ru-hexlet-io-courses_files/ru-hexlet-io-assets-application.css'],
+  ['ru-hexlet-io-courses_files/ru-hexlet-io-assets-professions-nodejs.png'],
+  ['ru-hexlet-io-courses_files/ru-hexlet-io-courses.html'],
+  ['ru-hexlet-io-courses_files/ru-hexlet-io-packs-js-runtime.js'],
+])('match actual and expected files: %s', async (filepath) => {
+  const actual = await readActualFile(filepath);
+  const expected = await readExpectedFile(filepath);
+  expect(actual).toBe(expected);
 });
