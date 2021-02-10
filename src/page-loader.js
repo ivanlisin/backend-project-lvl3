3c5a14c42701f4f9637e53654ec5fa49e3668c99
Ivan Lisin <ivanlisin.tech@gmail.com>
2021-02-10 21:04:17 +0700

    add page loader

--- src/page-loader.js
+++ src/page-loader.js
@@ -1,31 +1,46 @@
 // @ts-check
 
+import axios from 'axios';
 import path from 'path';
 import { promises as fs } from 'fs';
+import { makeDirNameByUrl, makeFileNameByUrl } from './name.js';
+import { getSourceLinks, replaceSrcLinksOnFilePaths } from './html.js';
 
-const getConfig = async (configName) => {
-  const configPath = path.join('__fixtures__', configName);
-  const data = await fs.readFile(configPath, 'utf8');
-  return JSON.parse(data);
-};
-
-const readExpectedFile = async (filepath) => {
-  const fixtureFilePath = path.join('__fixtures__', 'expected-dir', filepath);
-  return fs.readFile(fixtureFilePath, 'utf-8');
-};
-
-const simulatePageLoad = async (outDir) => {
-  const srcdir = 'ru-hexlet-io-courses_files';
-  const { filepaths } = await getConfig('page-loader-config.json');
-  await fs.mkdir(path.join(outDir, srcdir));
-  const promises = filepaths.map(async (filepath) => {
-    const data = await readExpectedFile(filepath);
-    const distpath = path.join(outDir, filepath);
-    await fs.writeFile(distpath, data);
+const downloadSourceFiles = (url, outputDir, html) => {
+  const { origin } = new URL(url);
+  const srcDirName = makeDirNameByUrl(url, origin);
+  const srcDirPath = path.join(outputDir, srcDirName);
+  return fs.mkdir(srcDirPath).then(() => {
+    const sourceLinks = getSourceLinks(html, origin);
+    const promises = sourceLinks.map((link) => {
+      const { href } = new URL(link, origin);
+      const filename = makeFileNameByUrl(link, origin);
+      const filepath = path.join(outputDir, srcDirName, filename);
+      return axios.get(href).then(({ data }) => fs.writeFile(filepath, data));
+    });
+    return Promise.all(promises);
   });
-  return Promise.all(promises);
 };
 
-const loadPage = (_url, outDir) => simulatePageLoad(outDir);
+const downloadIndexFile = (url, outputDir, html) => {
+  const { origin } = new URL(url);
+  const filename = makeFileNameByUrl(url, origin);
+  const filepath = path.join(outputDir, filename);
+  const updatedHtml = replaceSrcLinksOnFilePaths(html, origin, (link) => {
+    const srcDirName = makeDirNameByUrl(url, origin);
+    return path.join(srcDirName, makeFileNameByUrl(link, origin));
+  });
+  return fs.writeFile(filepath, updatedHtml);
+};
+
+const loadPage = async (url, outputDir) => axios.get(url).then((response) => {
+  const html = response.data;
+  const promises = [
+    downloadSourceFiles,
+    downloadIndexFile,
+  ].map((fn) => fn(url, outputDir, html));
+  // @ts-ignore
+  return Promise.all(promises);
+});
 
 export default loadPage;
