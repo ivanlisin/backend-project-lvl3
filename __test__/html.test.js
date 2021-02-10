4ada84c3656454371dcd19e7c11a2827d0a55336
Ivan Lisin <ivanlisin.tech@gmail.com>
2021-02-10 18:41:28 +0700

    update tests

--- __test__/html.test.js
+++ __test__/html.test.js
@@ -1,36 +1,45 @@
 import path from 'path';
 import { promises as fs } from 'fs';
-import { makeDirNameByUrl, makeFileNameByUrl } from '../src/name';
 import { getSourceLinks, replaceSrcLinksOnFilePaths } from '../src/html';
 
 const url = 'https://ru.hexlet.io/courses';
-const sourceLinks = [
-  '/assets/application.css',
-  '/assets/professions/nodejs.png',
-  '/courses',
-  'https://ru.hexlet.io/packs/js/runtime.js',
-];
 
-const readFixtureFile = async (...pathSegments) => {
-  const fixtureFilePath = path.join('__fixtures__', ...pathSegments);
-  return fs.readFile(fixtureFilePath, 'utf-8');
+const readFixtureFile = async (filename) => {
+  const filepath = path.join('__fixtures__', filename);
+  const data = await fs.readFile(filepath, 'utf-8');
+  return data.trim();
 };
 
 test('get source links from html', async () => {
-  const html = await readFixtureFile('index.html');
+  const sourceLinks = [
+    '/assets/application.css',
+    '/assets/professions/nodejs.png',
+    '/courses',
+    'https://ru.hexlet.io/packs/js/runtime.js',
+  ];
+  const html = await readFixtureFile('response-index.html');
   const { origin } = new URL(url);
-  const actual = getSourceLinks(html, origin);
-  const expected = sourceLinks;
-  expect(actual.sort()).toEqual(expected.sort());
+  const actual = getSourceLinks(html, origin).sort();
+  const expected = sourceLinks.sort();
+  expect(actual).toEqual(expected);
 });
 
-test('replace src links on filepaths', async () => {
-  const html = await readFixtureFile('index.html');
-  const { origin } = new URL(url);
-  const makeFilePathByUrl = (innerUrl) => {
-    const srcDir = makeDirNameByUrl(url, origin);
-    return path.join(srcDir, makeFileNameByUrl(innerUrl, origin));
+const makeFilePathByUrl = (link) => {
+  const srcDirName = 'ru-hexlet-io-courses_files';
+  const linkAndFileNameMapping = {
+    '/assets/application.css': 'ru-hexlet-io-assets-application.css',
+    '/assets/professions/nodejs.png': 'ru-hexlet-io-assets-professions-nodejs.png',
+    '/courses': 'ru-hexlet-io-courses.html',
+    'https://ru.hexlet.io/packs/js/runtime.js': 'ru-hexlet-io-packs-js-runtime.js',
   };
-  const expected = await readFixtureFile('expected-dir', 'ru-hexlet-io-courses.html');
-  expect(replaceSrcLinksOnFilePaths(html, origin, makeFilePathByUrl)).toEqual(expected);
+  const filename = linkAndFileNameMapping[link];
+  return path.join(srcDirName, filename);
+};
+
+test('replace src links on filepaths', async () => {
+  const html = await readFixtureFile('response-index.html');
+  const { origin } = new URL(url);
+  const actual = replaceSrcLinksOnFilePaths(html, origin, makeFilePathByUrl);
+  const expected = await readFixtureFile('local-index.html');
+  expect(actual).toEqual(expected);
 });
