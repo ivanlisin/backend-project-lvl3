fbfbc432816c88a962e8dfdc3cf5b8e793c5e926
Ivan Lisin <ivanlisin.tech@gmail.com>
2021-03-05 14:04:41 +0700

    update name.js

--- src/name.js
+++ src/name.js
@@ -3,24 +3,24 @@
 import _ from 'lodash';
 import path from 'path';
 
-const converUrlToKebabCase = (url, base = null, extname = '') => {
-  const normalizedBase = base === null ? _.noop() : base;
-  return new URL(url, normalizedBase).href
-    .replace(/.+:\/\//, '')
-    .replace(/\/$/, '')
-    .replace(extname, '')
-    .replace(/[^a-zA-Z0-9]+\b/gm, '-');
-};
-
 export const makeFileNameByUrl = (url, base = null) => {
-  const extname = path.extname(url);
-  const basename = converUrlToKebabCase(url, base, extname);
-  return extname === ''
-    ? `${basename}.html`
-    : `${basename}${extname}`;
+  const normalizedBase = base === null ? _.noop() : base;
+  const { origin, pathname } = new URL(url, normalizedBase);
+  const extname = path.extname(pathname);
+  const str = [
+    origin.replace(/http:\/\/|https:\/\//gm, ''),
+    pathname.replace(extname, ''),
+  ].join('-');
+  return `${_.kebabCase(str)}${extname}`;
 };
 
 export const makeDirNameByUrl = (url, base = null) => {
-  const dirname = converUrlToKebabCase(url, base);
-  return `${dirname}_files`;
+  const normalizedBase = base === null ? _.noop() : base;
+  const { origin, pathname } = new URL(url, normalizedBase);
+  const extname = path.extname(pathname);
+  const str = [
+    origin.replace(/http:\/\/|https:\/\//gm, ''),
+    pathname.replace(extname, ''),
+  ].join('-');
+  return `${_.kebabCase(str)}_files`;
 };
