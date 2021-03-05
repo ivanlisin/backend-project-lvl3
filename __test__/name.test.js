1bed181075b8dfffa4a7ebe61bce2c7c6115e1d7
Ivan Lisin <77109733+ivanlisin@users.noreply.github.com>
2021-03-05 14:04:41 +0700

    update name.js

--- __test__/name.test.js
+++ __test__/name.test.js
@@ -4,6 +4,7 @@ test.each([
   ['/assets/application.css', 'https://ru.hexlet.io/', 'ru-hexlet-io-assets-application.css'],
   ['/assets/professions/nodejs.png', 'https://ru.hexlet.io/', 'ru-hexlet-io-assets-professions-nodejs.png'],
   ['/packs/js/runtime.js', 'https://ru.hexlet.io/', 'ru-hexlet-io-packs-js-runtime.js'],
+  ['http://yandex.ru/', null, 'yandex-ru'],
 ])('makeFileNameByUrl with %s', (url, base, expected) => {
   expect(makeFileNameByUrl(url, base)).toBe(expected);
 });
@@ -11,6 +12,7 @@ test.each([
 test.each([
   ['https://ru.hexlet.io/courses', null, 'ru-hexlet-io-courses_files'],
   ['/courses', 'https://ru.hexlet.io/', 'ru-hexlet-io-courses_files'],
+  ['http://yandex.ru/', null, 'yandex-ru_files'],
 ])('makeDirNameByUrl with %s', (url, base, expected) => {
   expect(makeDirNameByUrl(url, base)).toBe(expected);
 });
