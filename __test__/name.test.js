93e7c6275e4f95b4f575c420433bc34422de2e99
Ivan Lisin <ivanlisin.tech@gmail.com>
2021-03-14 14:48:38 +0700

    update name.js

--- __test__/name.test.js
+++ __test__/name.test.js
@@ -1,9 +1,9 @@
 import { makeFileNameByUrl, makeDirNameByUrl } from '../src/name';
 
 test.each([
-  ['/assets/application.css', 'https://ru.hexlet.io/', 'ru-hexlet-io-assets-application.css'],
-  ['/assets/professions/nodejs.png', 'https://ru.hexlet.io/', 'ru-hexlet-io-assets-professions-nodejs.png'],
-  ['/packs/js/runtime.js', 'https://ru.hexlet.io/', 'ru-hexlet-io-packs-js-runtime.js'],
+  ['https://ru.hexlet.io/', '/assets/application.css', 'ru-hexlet-io-assets-application.css'],
+  ['https://ru.hexlet.io/', '/assets/professions/nodejs.png', 'ru-hexlet-io-assets-professions-nodejs.png'],
+  ['https://ru.hexlet.io/', '/packs/js/runtime.js', 'ru-hexlet-io-packs-js-runtime.js'],
   ['http://yandex.ru/', null, 'yandex-ru.html'],
 ])('makeFileNameByUrl with %s', (url, base, expected) => {
   expect(makeFileNameByUrl(url, base)).toBe(expected);
@@ -11,7 +11,7 @@ test.each([
 
 test.each([
   ['https://ru.hexlet.io/courses', null, 'ru-hexlet-io-courses_files'],
-  ['/courses', 'https://ru.hexlet.io/', 'ru-hexlet-io-courses_files'],
+  ['https://ru.hexlet.io/', '/courses', 'ru-hexlet-io-courses_files'],
   ['http://yandex.ru/', null, 'yandex-ru_files'],
 ])('makeDirNameByUrl with %s', (url, base, expected) => {
   expect(makeDirNameByUrl(url, base)).toBe(expected);
