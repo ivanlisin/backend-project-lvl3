c80bb12f31a1320a9f43a02b40e1600b9d63bc73
Ivan Lisin <77109733+ivanlisin@users.noreply.github.com>
2021-03-14 14:55:40 +0700

    update html.js

--- __test__/html.test.js
+++ __test__/html.test.js
@@ -1,9 +1,9 @@
 import path from 'path';
+import _ from 'lodash';
 import { promises as fs } from 'fs';
-import { getSourceLinks, replaceSrcLinksOnFilePaths } from '../src/html';
+import processAssetsLinks from '../src/html';
 
 const url = 'https://ru.hexlet.io/courses';
-const { origin } = new URL(url);
 
 let responseIndex;
 let localIndex;
@@ -19,40 +19,15 @@ beforeAll(async () => {
   localIndex = await readFixtureFile('local-index.html');
 });
 
-test('get source links from html', async () => {
-  const sourceLinks = [
-    '/assets/application.css',
-    '/assets/professions/nodejs.png',
-    '/courses',
-    'https://ru.hexlet.io/packs/js/runtime.js',
-  ];
-  const actual = getSourceLinks(responseIndex, origin).sort();
-  const expected = sourceLinks.sort();
-  expect(actual).toEqual(expected);
-});
+const assets = [
+  '/assets/application.css',
+  '/assets/professions/nodejs.png',
+  '/courses',
+  'https://ru.hexlet.io/packs/js/runtime.js',
+];
 
-const makeFilePathByUrl = (link) => {
-  const srcDirName = 'ru-hexlet-io-courses_files';
-  const linkAndFileNameMapping = {
-    '/assets/application.css': 'ru-hexlet-io-assets-application.css',
-    '/assets/professions/nodejs.png': 'ru-hexlet-io-assets-professions-nodejs.png',
-    '/courses': 'ru-hexlet-io-courses.html',
-    'https://ru.hexlet.io/packs/js/runtime.js': 'ru-hexlet-io-packs-js-runtime.js',
-  };
-  const filename = linkAndFileNameMapping[link];
-  return path.join(srcDirName, filename);
-};
-
-test('replace src links on filepaths', () => {
-  const actual = replaceSrcLinksOnFilePaths(responseIndex, origin, makeFilePathByUrl);
-  const expected = localIndex;
-  expect(actual).toEqual(expected);
-});
-
-test('replace src links on filepaths 2', () => {
-  const substr = ' href="https://cdn2.hexlet.io/assets/menu.css"';
-  const html = responseIndex.replace(substr, '');
-  const actual = replaceSrcLinksOnFilePaths(html, origin, makeFilePathByUrl);
-  const expected = localIndex.replace(substr, '');
-  expect(actual).toEqual(expected);
+test('processAssetsLinks 1', () => {
+  const data = processAssetsLinks(url, responseIndex);
+  expect(data.updatedHtml).toEqual(localIndex);
+  expect(data.assets.sort()).toEqual(assets.sort());
 });
